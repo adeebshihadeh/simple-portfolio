3f29d95ca72339c12b6e7a713a66b427d8087f81
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-12-17 15:38:43 -0800

    fadein title

--- animate.js
+++ animate.js
@@ -1,6 +1,8 @@
 function animate() {
   $(".card").hide();
 
+  $(".jumbotron").fadeIn("slow");
+
   $(".card").first().slideDown("slow", function next() {
     $(this).parent().next().next().children().first().slideDown("slow", next);
   });
